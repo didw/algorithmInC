c0603d7eebb6ba555575bc530e6afa20bbafe833
didw <yangjy0113@gmail.com>
2015-07-20 16:32:49 +0900

    changed a bit

--- 20150720/4_maxsum.c
+++ 20150720/4_maxsum.c
@@ -1,60 +1,97 @@
-
+// 4_maxsum.c
+// 수열에서 최대 부분 구간 합을 찾는 문제
+// a[] = {1, 4, -9, 2, 8, -1, 9, 3}
+// 1. 완전 탐색법 ( brute-force )
+#include <stdio.h>
 #define max(a, b) ((a) > (b) ? (a) : (b))
-#define min(a, b) ((a) < (b) ? (a) : (b))
-int maxSum1(int *a, int n) {
-	int res = -1e5;
-	for (int begin = 0; begin < n; ++begin) {
-		int sum = 0;
-		for (int end = begin; end < n; ++end) {
-			sum += a[end];
-			res = max(res, sum);
+
+// 시간 복잡도 : O(N^2)
+int maxSum(int *a, int n)
+{
+	// 2중 for문으로 구현하면 됩니다.
+	int sum = 0;		// 부분합
+	int ret = -9999;		// 부분합의 최대값
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		// sum값은 내부 루프 수행시 초기화 
+		sum = 0;
+		for (j = i; j < n; j++)
+		{
+			sum += a[j];
+			ret = max(ret, sum);
 		}
 	}
-	return res;
+	return ret;
 }
 
-int fastSum(int *a, int lo, int hi) {
+// 시간 복잡도
+// n * 재귀호출이 호출되는 횟수
+// O(nlogn)
+int _fastMaxSum(int *a, int lo, int hi)
+{
+	int left = -9999;	// 좌측 배열의 최대부분값
+	int right = -9999;	// 우측 배열의 최대부분값
+	int sum = 0;
+	int mid, single = 0;
+	int i, j;
+
+	// 탈출조건
 	if (lo == hi) return a[lo];
-	int mid = (lo + hi) / 2;
-	int single = max(fastSum(a, lo, mid), fastSum(a, mid + 1, hi));
-	int sum;
-	int left = 0;
-	sum = 0;
-	for (int i = mid; i >= lo; --i) {
+
+	// 배열을 2조각으로 나눈다.
+	mid = (lo + hi) / 2;
+
+	// 왼쪽구간의 최대값
+	for (i = mid; i >= lo; i--)
+	{
 		sum += a[i];
-		left = max(sum, left);
+		left = max(left, sum);
 	}
-	int right = 0;
+
 	sum = 0;
-	for (int i = mid+1; i <= hi; ++i) {
-		sum += a[i];
-		right = max(sum, right);
+	// 오른쪽구간의 최대값
+	for (j = mid+1; j <= hi; j++)
+	{
+		sum += a[j];
+		right = max(right, sum);
 	}
+
+	// 왼쪽 구간의 최대값과 오른쪽 구간의 최대값을 비교해서 큰 값을 취한다.
+	single = max(_fastMaxSum(a, lo, mid), _fastMaxSum(a, mid + 1, hi));
+
+	// 리턴해 주는 값은
+	// single 값과 전체 합산 값 중 큰것을 취한다.
 	return max(left + right, single);
 }
-int maxSum2(int *a, int n) {
-	return fastSum(a, 0, n);
+
+int fastMaxSum(int *a, int n)
+{
+	_fastMaxSum(a, 0, n - 1);
 }
 
-int maxSum3(int *a, int n) {
-	int *subsum = (int*)malloc(sizeof(int)*n);
-	subsum[0] = a[0];
-	for (int i = 1; i < n; ++i) {
-		subsum[i] = subsum[i - 1] + a[i];
+// maxAt(i) = max(0, maxAt(i-1)) + A[i]
+// 시간복잡도 : O(n)
+int fastestMaxSum(int *a, int n)
+{
+	int sum = 0;
+	int ret = -9999;
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		sum = max(0, sum) + a[i];
+		ret = max(ret, sum);
 	}
-	int maxvalue = -1e5, minvalue = 1e5;
-	for (int i = 0; i < n; ++i) {
-		maxvalue = max(maxvalue, subsum[i]);
-		minvalue = min(minvalue, subsum[i]);
-	}
-	return maxvalue - minvalue;
+	return ret;
 }
 
-int main() {
+int main()
+{
 	int a[] = { 1, 4, -9, 2, 8, -1, 9, 3 };
-	int n = sizeof(a) / sizeof(a[0]);
-	printf("maxsum: %d\n", maxSum1(a, n));
-	printf("maxsum: %d\n", maxSum2(a, n));
-	printf("maxsum: %d\n", maxSum3(a, n));
+	int n = sizeof(a) / sizeof(a[0]);	// 배열 원소의 갯수
+	printf("maxSum : %d\n", maxSum(a, n));
+	printf("fastMaxSum : %d\n", fastMaxSum(a, n));
+	printf("fastestMaxSum : %d\n", fastestMaxSum(a, n));
 	return 0;
 }
